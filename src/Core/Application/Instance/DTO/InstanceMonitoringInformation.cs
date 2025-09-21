90f750c35ff5c72770ed3dfe57a39456768537cd
arseniiz <shrayky@au124.ru>
2025-09-21 11:40:21 +0700

    Добавил имена для json полей

--- src/Core/Application/Instance/DTO/InstanceMonitoringInformation.cs
+++ src/Core/Application/Instance/DTO/InstanceMonitoringInformation.cs
@@ -5,12 +5,12 @@ namespace Application.Instance.DTO;
 public record InstanceMonitoringInformation
 {
     [JsonPropertyName("name")]
-    public string Name = string.Empty;
-    [JsonPropertyName("token")]
-    public string Token = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    [JsonPropertyName("id")]
+    public string Token { get; init; } = string.Empty;
     [JsonPropertyName("version")]
-    public string Version = string.Empty;
+    public string Version { get; init; } = string.Empty;
     [JsonPropertyName("lastUpdated")]
-    public DateTime LastUpdated = DateTime.MinValue;
+    public DateTime LastUpdated { get; init; } = DateTime.MinValue;
     
 }
