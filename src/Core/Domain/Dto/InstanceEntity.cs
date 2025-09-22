3f29add75dec7662f1fed8d51579dfb90f3e36f3
arseniiz <shrayky@au124.ru>
2025-09-22 15:42:03 +0700

    Добавлен секретный ключ для шифрования пакетов от fmu-api

--- src/Core/Domain/Dto/InstanceEntity.cs
+++ src/Core/Domain/Dto/InstanceEntity.cs
@@ -9,6 +9,7 @@ public class InstanceEntity : IHaveStringId
     public string Name { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public string SecretKey { get; set; } = string.Empty;
     public FmuApiParameters Settings { get; set; } = new();
     public List<CdnData> Cdn { get; set; } = [];
     public object LocalModules { get; set; } = new object();
