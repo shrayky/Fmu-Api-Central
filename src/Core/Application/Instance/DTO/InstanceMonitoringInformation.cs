11ecdb319d91ccf31253c679d2716190f77c8023
arseniiz <shrayky@au124.ru>
2025-09-29 10:52:37 +0700

    в dto инстанса добавлен секретный ключ

--- src/Core/Application/Instance/DTO/InstanceMonitoringInformation.cs
+++ src/Core/Application/Instance/DTO/InstanceMonitoringInformation.cs
@@ -6,11 +6,17 @@ public record InstanceMonitoringInformation
 {
     [JsonPropertyName("name")]
     public string Name { get; init; } = string.Empty;
+    
     [JsonPropertyName("id")]
     public string Token { get; init; } = string.Empty;
+    
     [JsonPropertyName("version")]
     public string Version { get; init; } = string.Empty;
+    
     [JsonPropertyName("lastUpdated")]
     public DateTime LastUpdated { get; init; } = DateTime.MinValue;
+    
+    [JsonPropertyName("secretKey")]
+    public string SecretKey { get; init; } = string.Empty;
     
 }
