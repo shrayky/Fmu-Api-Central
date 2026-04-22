4507298594398f26f98e1270eaea7fcca24d10af
arseniiz <shrayky@au124.ru>
2026-04-22 10:47:37 +0700

    Отсутсвовали

---
+++ src/Core/Domain/Logs/Dto/LogPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Logs.Dto
+{
+    public record LogPacket
+    {
+        public List<string> LogFileNames { get; set; } = [];
+        public string SelectedLogFileName {  get; set; } = string.Empty;
+        public string LogText {  get; set; } = string.Empty;
+    }
+}
